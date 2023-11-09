f4ef130989cbf17846563fd0749e98eea31ff8e2
miroslaw-mroczkowski <97404193+miroslaw-mroczkowski@users.noreply.github.com>
2023-11-09 20:40:55 +0100

    All tablet and desktop design done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,26 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	content: ['./index.html', './src/*.{html,js}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				jakarta: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
-			},
-			colors: {
-				customPrimaryDark: '#13183f',
-				customPrimaryLight: '#83869a',
-				customBlue: '#666ca3',
-				customPink: '#f74780',
-				customPinkLight: '#ffa7c3',
-				customGradientOrange: '#ff6f48',
-				customGradientBlue: '#4851ff',
-				customGradientPink: '#f02aa6',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./index.html', './src/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        jakarta: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        customPrimaryDark: '#13183f',
+        customPrimaryLight: '#83869a',
+        customBlue: '#666ca3',
+        customPink: '#f74780',
+        customPinkLight: '#ffa7c3',
+        customGradientOrange: '#ff6f48',
+        customGradientBlue: '#4851ff',
+        customGradientPink: '#f02aa6',
+      },
+      gridTemplateColumns: {
+        fluid: 'repeat(auto-fit, minmax(min(100%, 280px), 1fr)))',
+      },
+    },
+  },
+  plugins: [],
 };
